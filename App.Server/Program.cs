6da3cb257c59d96267af7df900d1a86748045fd3
Paul Krisadaolarn <pauly.k@gmail.com>
2022-08-10 14:13:57 +0700

    Add Route Parameters example

--- App.Server/Program.cs
+++ App.Server/Program.cs
@@ -9,17 +9,16 @@ var config = new ConfigurationBuilder()
     .AddEnvironmentVariables()
     .Build();
 
-var x = config["Todo:1"];
-Console.WriteLine(x);
-
 builder.Services.AddSingleton<Greeter>();
 
 var app = builder.Build();
+
+var getTodo = (string id) => config[$"todo:{id}"];
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/greet", (Greeter greeter) => greeter.Greet);
 app.MapGet("/app-title", () => config["AppTitle"] ?? "No title");
 app.MapGet("/connection-string", () => config.GetConnectionString("Default"));
-// app.MapGet("/todo:id")
+ app.MapGet("/todo/{id}", (string id) => getTodo(id));
 
-app.Run("http://localhost:8000");
+app.Run();
